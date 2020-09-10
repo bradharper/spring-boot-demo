126092afd5b932841d51fc098246631640d0a897
Brad Harper <bradharper@gmail.com>
2020-09-10 19:44:32 -0400

    environment var dump

--- src/main/java/example/smallest/controllers/WelcomeController.java
+++ src/main/java/example/smallest/controllers/WelcomeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 @Controller
 public class WelcomeController {
 
@@ -19,7 +21,14 @@ public class WelcomeController {
 	@RequestMapping(method = RequestMethod.GET, produces = {"application/json"})
 	public @ResponseBody String helloWorld() {
 		
+		StringBuffer allEnvironmentVars = new StringBuffer();
 
-		return "*** Spring Boot Demo *** \n"+propertyMessage+"\n"+environmentVariableMessage;
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+			allEnvironmentVars.append(envName).append(" = ").append(env.get(envName)).append("\n");
+		}
+
+		return "*** Spring Boot Demo *** \n"+propertyMessage+"\n"+environmentVariableMessage+"\n\n\n"
+				+allEnvironmentVars.toString();
 	}
 }
